c59b3b1f6a85d0bfe14d74478f1abccf4ed10034
shyamaprasdk <shyamaprasad991@gmail.com>
2023-05-20 23:19:27 +0530

    Rename

--- ios/dailyquotesTests/dailyquotesTests.m
+++ ios/dailyquotesTests/dailyquotesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface dailyquotesTests : XCTestCase
+@interface DailyQuotesTests : XCTestCase
 
 @end
 
-@implementation dailyquotesTests
+@implementation DailyQuotesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
